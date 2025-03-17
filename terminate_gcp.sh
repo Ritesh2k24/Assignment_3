3a2debe859ccd90fe19da21920622f9173aa7e2a
Ritesh2k24 <162950856+Ritesh2k24@users.noreply.github.com>
2025-03-17 11:52:58 +0530

    Update terminate_gcp.sh

--- terminate_gcp.sh
+++ terminate_gcp.sh
@@ -2,7 +2,7 @@
 
 INSTANCE_NAME="flask-instance"
 ZONE="us-central1-a"
-PROJECT="your-gcp-project-id"
+PROJECT="assignment-3-vcc"
 
 echo "Stopping and deleting GCP instance..."
 gcloud compute instances delete $INSTANCE_NAME --zone $ZONE --project $PROJECT --quiet
